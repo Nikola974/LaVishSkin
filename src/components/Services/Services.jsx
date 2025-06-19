142f0f4a149bdd4e90a9706d6e39007d98f71045
Nikola Dobrev <NikolaDobrev974@gmail.com>
2025-06-20 01:34:14 +0300

    Featurable carousel and other pictures in services

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,138 +1,72 @@
 import { useState } from "react";
+import { ReactGoogleReviews } from "react-google-reviews";
+import "react-google-reviews/dist/index.css";
 import styles from "./Services.module.css";
-import malesImage from "../../assets/IMG_7307.jpeg";
-import femalesImage from "../../assets/review.jpg";
+import malesImage from "../../assets/candela.jpg";
+import femalesImage from "../../assets/femalesImage.jpg";
 import ServicesOptions from "./ServicesOptions.jsx";
+import { servicesData } from './ServiceData.js'
 
-const servicesData = {
-  female: [
-    {
-      title: "Зона – Лице",
-      items: [
-        "Горна устна - 25",
-        "Бакенбарди - 25",
-        "Брадичка - 25",
-        "Врат - 30",
-        "Скули - 25",
-        "Шия - 30",
-        "Цяло лице - 65",
-      ],
-    },
-    {
-      title: "Зона – Тяло",
-      items: [
-        "Гърди - 70",
-        "Ареоли - 25",
-        "Корем - 75",
-        "Корем лента - 40",
-        "Гръб - 120",
-        "Кръст - 65",
-        "Подмишници - 40",
-        "Предмишници - 60",
-        "Рамене - 60",
-        "Цели ръце - 100",
-        "Цяло тяло - 600",
-      ],
-    },
-    {
-      title: "Зона – Крака",
-      items: [
-        "Интим - 100",
-        "Бикини линия - 50",
-        "Седалище - 75",
-        "Бедра - 150",
-        "Подбедрици - 120",
-        "Цели крака - 240",
-        "Пръсти - 25",
-      ],
-    },
-  ],
-  male: [
-    {
-      title: "Зона – Лице",
-      items: [
-        "Брада - 30",
-        "Бакенбарди - 30",
-        "Врат - 50",
-        "Горна устна - 30",
-        "Скули - 30",
-        "Шия - 30",
-        "Чело и вежди - 40",
-      ],
-    },
-    {
-      title: "Зона – Тяло",
-      items: [
-        "Гърди - 90",
-        "Ареоли - 35",
-        "Корем - 90",
-        "Корем лента - 40",
-        "Гръб - 150",
-        "Кръст - 90",
-        "Подмишници - 40",
-        "Предмишници - 80",
-        "Рамене - 80",
-        "Цели ръце - 150",
-        "Цяло тяло - 1100",
-      ],
-    },
-    {
-      title: "Зона – Крака",
-      items: [
-        "Слабини - 120",
-        "Седалище - 90",
-        "Бедра - 150",
-        "Подбедрици - 150",
-        "Цели крака - 320",
-      ],
-    },
-  ],
-};
+function Reviews() {
+  const featurableWidgetId = "fdfd5a65-a78b-4d7d-b9e8-84484b13b639";
 
+  return (
+    <ReactGoogleReviews layout="carousel" featurableId={featurableWidgetId} />
+  );
+}
 
 export default function Services() {
   const [activeGender, setActiveGender] = useState("female");
 
-
   return (
-    
-    <div className={styles.servicesContainer}>
-      <div className={styles.imageContainer}>
-      <img
-          src={femalesImage}
-          alt="Жени"
-          className={`${styles.image} ${activeGender === "female" ? styles.active : ""}`}
-        />
-        <img
-          src={malesImage}
-          alt="Мъже"
-          className={`${styles.image} ${activeGender === "male" ? styles.active : ""}`}
-        />
-      </div>
-
-      <div className={styles.servicesInfoContainer}>
-        <h2>Процедури</h2>
-        <div className={styles.toggleButtons}>
-          <button
-            className={activeGender === "female" ? styles.active : ""}
-            onClick={() => setActiveGender("female")}
-          >
-            Лазерна Епилация Жени
-          </button>
-          <button
-            className={activeGender === "male" ? styles.active : ""}
-            onClick={() => setActiveGender("male")}
-          >
-            Лазерна Епилация Мъже
-          </button>
+    <div className={styles.services}>
+      <div className={styles.servicesContainer}>
+        <div className={styles.imageContainer}>
+          <img
+            src={femalesImage}
+            alt="Жени"
+            className={`${styles.image} ${
+              activeGender === "female" ? styles.active : ""
+            }`}
+          />
+          <img
+            src={malesImage}
+            alt="Мъже"
+            className={`${styles.image} ${
+              activeGender === "male" ? styles.active : ""
+            }`}
+          />
         </div>
 
-        <ul className={styles.priceList}>
-  {servicesData[activeGender].map((section, index) => (
-    <ServicesOptions key={index} title={section.title} items={section.items} />
-  ))}
-</ul>
+        <div className={styles.servicesInfoContainer}>
+          <h2>Процедури</h2>
+          <div className={styles.toggleButtons}>
+            <button
+              className={activeGender === "female" ? styles.active : ""}
+              onClick={() => setActiveGender("female")}
+            >
+              Лазерна Епилация Жени
+            </button>
+            <button
+              className={activeGender === "male" ? styles.active : ""}
+              onClick={() => setActiveGender("male")}
+            >
+              Лазерна Епилация Мъже
+            </button>
+          </div>
+
+          <ul className={styles.priceList}>
+            {servicesData[activeGender].map((section, index) => (
+              <ServicesOptions
+                key={index}
+                title={section.title}
+                items={section.items}
+              />
+            ))}
+          </ul>
+        </div>
       </div>
+      <Reviews />
     </div>
   );
 }
