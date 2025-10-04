acd90b9b7b9f57e9dc6a1e1be8f64509b27f55e3
Nikola Dobrev <NikolaDobrev974@gmail.com>
2025-10-04 18:24:27 +0300

    adding pop up image on overlay

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import NavigationBar from "./components/Navigation/NavigationBar.jsx";
 import AboutUs from "./components/AboutUs/AboutUs.jsx";
 import Services from "./components/Services/Services.jsx";
 import Contacts from "./components/Contacts/Contacts.jsx";
+import Popup from "./components/PopUpImage/PopUpImage.jsx";
+import promoImage from "./assets/popup.png";
+
 
 function App() {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -27,6 +30,7 @@ function App() {
   return (
     <div className={styles.App}>
       <ScrollToTop />
+      <Popup image={promoImage} />
       <div className={stylesNav.container}>
         <NavigationBar
           scrollToSection={scrollToSection}
---
+++ src/components/PopUpImage/PopUpImg.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import styles from "./Popup.module.css";
+import promoImage from "../../assets/popup.png";
+
+export default function Popup() {
+  const [showPopup, setShowPopup] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowPopup(true);
+    }, 2000); // 2 seconds
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!showPopup) return null;
+
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.popup}>
+        <button className={styles.closeBtn} onClick={() => setShowPopup(false)}>
+          ✕
+        </button>
+        <img src={promoImage} alt="Promo" className={styles.image} />
+      </div>
+    </div>
+  );
+}
